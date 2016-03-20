d097a9fa2251b07148e7f5f20f29f0b6a8e48b32
Dommert <DommertEnterprises@Gmail.com>
2016-03-19 23:03:51 -0700

    base setup

---
+++ site/assets/js/routes.js
@@ -0,0 +1,18 @@
+var foundationRoutes = [
+	{"name":"settings",
+	"url":"/settings-this-is-cool",
+	"animationIn":"slideFromLeft",
+	"animationOut":"slideFromLeft",
+	"path":"templates/settings.html"
+	},
+	{"name":"home",
+	"url":"/home",
+	"animationIn":"slideFromRight",
+	"path":"templates/home.html"
+	},
+	{"name":"frontpage",
+	"url":"/",
+	"animationIn":"slideFromRight",
+	"path":"templates/frontpage.html"
+	}
+	]; 
